92018e440e385f81c9e9d1be50e2fb1e9b75fead
dadi11 <dadi.sikorski@gmail.com>
2018-05-16 21:20:57 +0200

    oreo update

--- omni_mido.mk
+++ omni_mido.mk
@@ -30,10 +30,6 @@ PRODUCT_PACKAGES += \
 	charger_res_images \
 	charger
 
-# Time Zone data for recovery
-PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
 PRODUCT_NAME := omni_mido
