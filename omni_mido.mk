c6cb799453af1c1ca11a5f738ac4f1ef20648bf2
dianlujitao <dianlujitao@lineageos.org>
2019-03-23 16:17:36 +0800

    mido: Fix HW FDE when building with Pie tree
     * ro.board.platform is no longer part of recovery ramdisk on Pie, thus
       hw_get_module_by_class would fail to find the keystore module and HW
       FDE is broken.
     * To address the issue, use a property to specify the HAL class to make
       the HAL picked by libhardware.
    
    Change-Id: I2b09a06be34ca66a400e98fce8714226ae5f3d10
    Signed-off-by: Albert I <kras@raphielgang.org>

--- omni_mido.mk
+++ omni_mido.mk
@@ -22,6 +22,9 @@ $(call inherit-product, build/target/product/embedded.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=msm8953
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
 PRODUCT_NAME := omni_mido
