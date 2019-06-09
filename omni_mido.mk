114c63e44e8df6db1bd736f87f4d7bc2e054fbbe
Albert I <kras@raphielgang.org>
2019-06-09 22:21:58 +0800

    mido: Cleanup for P
    Signed-off-by: Albert I <kras@raphielgang.org>

--- omni_mido.mk
+++ omni_mido.mk
@@ -14,27 +14,15 @@
 # limitations under the License.
 #
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Release name
+PRODUCT_RELEASE_NAME := mido
 
-# Inherit some common Omni stuff.
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit Telephony packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit language packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Charger
-#PRODUCT_PACKAGES += \
-    charger_res_images
-
-# Time Zone data for recovery
-#PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
-
-## Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
 PRODUCT_NAME := omni_mido
 PRODUCT_BRAND := Xiaomi
